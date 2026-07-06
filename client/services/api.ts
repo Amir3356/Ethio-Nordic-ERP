1aa66ade64bc4561c00fbcd67cc54b7d4a7a63ea
Amir3356 <amirsiraj1995@gmail.com>
2026-07-06 14:45:32 +0300

    feat: Remove baseURL from Axios instance configuration

--- client/services/api.ts
+++ client/services/api.ts
@@ -1,7 +1,6 @@
 import axios, { type AxiosInstance, type InternalAxiosRequestConfig, type AxiosResponse } from 'axios';
 
 export const api: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
